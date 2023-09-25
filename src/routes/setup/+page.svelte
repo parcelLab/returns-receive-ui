f880c41acaad7fb08487874f6c1122355bad7939
Julian Krenge <julian@parcellab.com>
2023-09-25 21:02:43 +0200

    fix: assign 2y lifetime to cookies

--- src/routes/setup/+page.svelte
+++ src/routes/setup/+page.svelte
@@ -22,7 +22,7 @@
 	}
 </script>
 
-<div class="overflow-hidden rounded-md bg-white shadow m-8">
+<div class="overflow-hidden rounded-md bg-white shadow m-8 max-w-4xl mx-auto">
 	<div class="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
 		<div class="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap">
 			<div class="ml-4 mt-2">
