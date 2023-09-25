70b3fe4525e33923b5cce2bed1840d9473440336
Julian Krenge <julian@parcellab.com>
2023-09-25 21:07:42 +0200

    fix: add base path

--- src/routes/auto-setup/+page.svelte
+++ src/routes/auto-setup/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { base } from '$app/paths';
 	import { createCookie } from '$lib/cookie';
 	import { onMount } from 'svelte';
 
@@ -17,7 +18,7 @@
 
 		function countdown() {
 			if (secondsUntilRedirect === 0) {
-				window.location.href = '/';
+				window.location.href = `${base}/`;
 			} else {
 				setTimeout(() => {
 					secondsUntilRedirect--;
@@ -63,7 +64,7 @@
 			</div>
 			<div class="ml-4 mt-2 flex-shrink-0">
 				<a
-					href="/"
+					href="{base}/"
 					type="button"
 					class="relative inline-flex items-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
 				>
